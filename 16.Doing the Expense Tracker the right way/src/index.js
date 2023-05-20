a96a935fd35f3b14b91e3537d88326fce70c622e
Mohammad Ayub Ansari <119043948+MohammadAyubAnsari@users.noreply.github.com>
2023-05-20 09:55:44 +0530

    16.Doing the Expense Tracker the right way

---
+++ 16.Doing the Expense Tracker the right way/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import ContextProvider from "./components/Context/AppContext";
+import { Provider } from "react-redux";
+import store from "./components/States/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <React.StrictMode>
+  <ContextProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ContextProvider>
+
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
